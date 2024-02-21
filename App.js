389c0facf313e0f7864e6179bbe226d6382d5c7d
nammlt <fansssexo@gmail.com>
2024-02-21 08:24:38 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import Welcome from './screens/Welcome';
+import Ex01 from './screens/week2/Ex01';
+import Ex02 from './screens/week2/Ex02';
+import Ex03 from './screens/week2/Ex03';
+import EX04 from './screens/week2/Ex04';
+import Ex05 from './screens/week2/Ex05';
+import Ex06 from './screens/week2/Ex06';
+import Ex07 from './screens/week2/Ex07';
+import Ex08 from './screens/week2/Ex08';
+import Ex09 from './screens/week2/Ex09';
+import Ex10 from './screens/week2/Ex10';
+import Ex11 from './screens/week2/Ex11';
+import Ex12 from './screens/week2/Ex12';
+import Extra from './screens/week2/Extra';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Welcome />
+    // <Ex01 />
+    // <Ex02 />
+    // <Ex03 />
+    // <EX04 />
+    // <Ex05 />
+    // <Ex06 />
+    // <Ex07 />
+    // <Ex08 />
+    // <Ex09 />
+    // <Ex10 />
+    // <Ex11 />
+    // <Ex12 />
+    <Extra />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ screens/week2/Extra.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Extra() {
+  return (
+    <View style={{ flex: 1, flexDirection: 'column'}}>
+        <View style={{flex : 1 , backgroundColor: '#FFEBB6'}}></View>
+            <View style={{flex : 1 , flexDirection: 'row'}}>
+                <View style={{flex : 1 , backgroundColor: '#8BD7B1'}}></View>
+                <View style={{flex : 2 , backgroundColor: '#FE706E'}}></View>
+                    {/* <View style={{flex : 1 , flexDirection: 'row'}}>
+                        <View style={{flex : 1 , backgroundColor: '#8BD7B1'}}></View>
+                        <View style={{flex : 2 , backgroundColor: '#FFCB65'}}></View>
+                    </View> */}
+            </View>
+        <View style={{flex : 2 , backgroundColor: '#8BD7B1'}}></View>
+            
+    </View>
+  );
+}
